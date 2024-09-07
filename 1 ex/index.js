1496c0816493118a761dd17480ae43a30cb61cce
PIotr Stefanski <peterstefanski3@gmail.com>
2024-09-07 23:35:46 +0200

    homework_01

---
+++ 1 ex/index.js
@@ -0,0 +1,39 @@
+// Here are some exercises to be done for next week. If you have them before send them back and i'll check them.
+
+// Exercise 1: Using the + Operator
+// Task: Create two variables, greeting with the value "Hello" and name with the value "John". Concatenate these two variables using the + operator to form the message "Hello, John!", and log it to the console.
+
+const greeting = "Hello";
+const name = "John";
+
+console.log(greeting + " " + name);
+
+// Exercise 2: Template Literals (` `)
+// Task: Create variables city with the value "New York" and weather with the value "sunny". Use template literals to create the sentence "The weather in New York is sunny today.", and log it to the console.
+
+const city = "New Your";
+const weather = "sunny";
+console.log(`The weather in ${city} is ${weather} today.`);
+
+// Exercise 3: Concatenating Multiple Strings with +
+// Task: Create variables firstName, middleName, and lastName with values "Anna", "Marie", and "Smith", respectively. Use the + operator to concatenate these into the full name "Anna Marie Smith", including spaces, and log it to the console.
+
+const firstName = "Anna";
+const middleName = "Marie";
+const lastName = "Smith";
+
+console.log(firstName + " " + middleName + " " + lastName);
+// Exercise 4: Mixing Strings and Variables with Template Literals
+// Task: Create variables product with the value "laptop", price with the value 1200, and currency with the value "USD". Use template literals to create the message "The price of the laptop is 1200 USD.", and log it to the console.
+
+const product = "laptop";
+const price = "1200";
+const currency = "USD";
+console.log(`The price of the ${product} is ${price} ${currency}.`);
+
+// Exercise 5: Combining Strings and Numbers
+// Task: Create a variable quantity with the value 5 and a variable item with the value "apples". Use the + operator to create the message "You have 5 apples.", and log it to the console.
+
+const quantity = 5;
+const item = "apples";
+console.log("You" + " " + "have" + " " + quantity + " " + item);
